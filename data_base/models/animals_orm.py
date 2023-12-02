4a3478feece7153d21ee945ddcf2fabd22a377ea
Lucas-PC <lgbs@cesar.school>
2023-12-02 18:15:44 -0300

    feat: create models for animals and species

---
+++ data_base/models/animals_orm.py
@@ -0,0 +1,17 @@
+'''module that contains the class that represents the animals table in the database'''
+from sqlalchemy import Column, Integer, String, ForeignKey
+from data_base.configs.base import Base
+
+class Animal(Base):
+    '''class that represents the animals table in the database'''
+    __tablename__ = 'animals'
+
+    id = Column(Integer, primary_key=True)
+    surname = Column(String(50), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String(1), nullable=False)
+    specie = Column(String(50), ForeignKey('species.name'), nullable=False)
+
+    def __repr__(self):
+        return f'Animal: {self.surname} Age: {self.age} Gender: {self.gender} Specie: {self.specie}'
+    
---
+++ data_base/models/specie_orm.py
@@ -0,0 +1,15 @@
+'''module that contains the class that represents the species table in the database'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from data_base.configs.base import Base
+
+class Specie(Base):
+    '''class that represents the species table in the database'''
+    __tablename__ = 'species'
+
+    id = Column(Integer, primary_key=True)
+    specie_name = Column(String(50), nullable=False)
+    animals = relationship('Animal', backref='specie', lazy='subquery')
+
+    def __repr__(self):
+        return f'Specie: {self.specie_name}'
